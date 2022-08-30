9b5979cdd61115f1a81fd6feb8ed7966aeba860a
William <wms274@nyu.edu>
2022-08-30 17:47:45 -0400

    commit tree aca7b96a7acd01ee3555226a5937e897fe62c365

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Will is the champion'
+document.querySelector('body').append(newHeader)
